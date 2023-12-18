14e1d34bf1d97a9597a294dfecd4e9213836e2eb
Jose Carlos Izquierdo <jcarlosillice@gmail.com>
2023-12-18 21:10:15 +0100

    Sistema de millora al salt de la Nixie

--- AqualungUnity/Assets/Scripts/Mechanics/CheckGround.cs
+++ AqualungUnity/Assets/Scripts/Mechanics/CheckGround.cs
@@ -1,19 +1,37 @@
+using Platformer.Mechanics;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CheckGround : MonoBehaviour
 {
+    
     // Es fa la variable com a static. Així es pot utilitzar en un altre Script
     public static bool isGrounded;
 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        Health health = collision.gameObject.GetComponent<Health>();
+        EnemyController enemyController = GetComponent<EnemyController>();
+
+        // comprovar si el gameObject amb el qual col·lisiona té el tag "Enemic"
+        if (collision.gameObject.CompareTag("Enemic"))
+        {
+
+            //Verificar si l'enemic té vida
+            if (health != null && enemyController != null)
+            {
+                health.Decrement();
+            }
+            
+        }
+
         if (collision.gameObject.layer == LayerMask.NameToLayer("Level"))
         {
             isGrounded = true;
         }
+
     }
 
     private void OnTriggerExit2D(Collider2D collision)
