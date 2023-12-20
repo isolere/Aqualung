9e4695109090e867b771cac8ac34a00eea1d95d3
GuillemPlanas <planas_g@hotmail.com>
2023-12-20 20:55:06 +0100

    solucionat salt/suicidi enemics/ajustades algunes tipografies

--- AqualungUnity/Assets/Scripts/Mechanics/CheckGround.cs
+++ AqualungUnity/Assets/Scripts/Mechanics/CheckGround.cs
@@ -12,26 +12,10 @@ public class CheckGround : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Health health = collision.gameObject.GetComponent<Health>();
-        EnemyController enemyController = GetComponent<EnemyController>();
-
-        // comprovar si el gameObject amb el qual col·lisiona té el tag "Enemic"
-        if (collision.gameObject.CompareTag("Enemic"))
-        {
-
-            //Verificar si l'enemic té vida
-            if (health != null && enemyController != null)
-            {
-                health.Decrement(false);
-            }
-            
-        }
-
         if (collision.gameObject.layer == LayerMask.NameToLayer("Level"))
         {
             isGrounded = true;
         }
-
     }
 
     private void OnTriggerExit2D(Collider2D collision)
